4431d26a9281bd2e79ec5af5449d1ab4b3598dd5
Jose Zavala <jzavala@rocketlawyer.com>
2018-09-20 13:31:30 -0500

    get a todo by its id

--- controllers/todo.py
+++ controllers/todo.py
@@ -12,6 +12,14 @@ def create_todo(todo_payload):
     return todo_repository.create_todo(todo_payload=todo_payload)
 
 
+#
+#  Get a resource by its id
+#
+def get_todo(todo_id):
+    return todo_repository.get_todo_by_id(todo_id=todo_id)
+
+
 routes = [
-    Route('/todos', method='POST', handler=create_todo)
+    Route('/todos', method='POST', handler=create_todo),
+    Route('/todos/{todo_id}', method='GET', handler=get_todo)
 ]
--- repositories/todo.py
+++ repositories/todo.py
@@ -7,10 +7,16 @@ class TodoRepository(object):
 
 
     #
-    # Create a new resource
+    #  Create a new resource
     #
     def create_todo(self, todo_payload):
         todo_id = str(uuid.uuid4())
         self.todos_db[todo_id] = todo_payload
         todo_payload['id'] = todo_id
         return todo_payload
+
+    #
+    #  Get a resource by its id
+    #
+    def get_todo_by_id(self, todo_id):
+        return self.todos_db[todo_id]
